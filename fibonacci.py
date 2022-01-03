75eb79039849acb3ba8a9f0350790234dbf8e2c0
Renan Zanesco Comoli <37589475+enedois@users.noreply.github.com>
2022-01-03 00:17:12 -0300

    Initial commit

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+def fibonacci(jumps):
+    fibo_seq=[]
+    for i in range(jumps):  
+        #print("iteração: "+str(i))      
+        if len(fibo_seq)>=2:
+            #print("ultimo"+str(fibo_seq[len(fibo_seq)-1]))
+            newnumber = fibo_seq[len(fibo_seq)-1] + fibo_seq[len(fibo_seq)-2]
+            print(newnumber)
+            fibo_seq.append(newnumber)
+        else:
+            print(i)
+            fibo_seq.append(i)
+
+
+
+fibonacci(10001)
